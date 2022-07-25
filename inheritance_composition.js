1dd0a4e76dc6ac8707af8651ff5856925e326cb9
DelowerHossainRU <delowerhossainsaide333@gmail.com>
2022-07-25 17:17:41 +0600

    first change

--- inheritance_composition.js
+++ inheritance_composition.js
@@ -30,31 +30,25 @@ var canSex = {
     }
 }
 
-//Inheritance:
-// var person = Object.assign({}, canEat, canWalk, canSwim)
-// person.name = "Delower"
-// console.log(person)
+// Inheritance:
+var person = Object.assign({}, canEat, canWalk, canSwim)
+person.name = "Delower"
+console.log(person)
 
-//Composition:
+// Composition:
 
-// function Person(name){
-//     this.name = name
-// }
-
-// // Object.assign(Person.prototype, canEat, canSwim, canWalk)
-// // var p1 = new Person("Delower")
-// // p1.Eat()
-// // console.log(p1)
-
-// mixin(Person.prototype, canEat, canWalk)
-
-// var person = new Person("Delower")
-// person.Walk()
-
-function alien(name){
+function Person(name){
     this.name = name
 }
-mixin(alien.prototype, canSex, canSleep)
-var aliensharif = new alien("Sharif")
 
-aliensharif.Sex()
+// Object.assign(Person.prototype, canEat, canSwim, canWalk)
+// var p1 = new Person("Delower")
+// p1.Eat()
+// console.log(p1)
+
+mixin(Person.prototype, canEat, canWalk)
+
+var person = new Person("Delower")
+person.Walk()
+
+
